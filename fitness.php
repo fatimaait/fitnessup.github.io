ba369a98456bb4a732dd83c430e6ec84c8c5268d
fatimaait <34036086+fatimaait@users.noreply.github.com>
2021-10-03 18:41:56 +0100

    Add files via upload

---
+++ fitness.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> FITNESSUP </title>
+	<link rel="stylesheet" media="screen" href="css/style.css">
+	<link rel="stylesheet" media="screen" href="css/style.scss">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Dave Shea">
+	<meta name="description" content="stack">
+	<meta name="robots" content="all">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.5.0/css/all.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    
+</head>
+<body>
+<script type="">$(document).ready(function() {
+    $(".flex-item2").animate({left: "+=500"}, 1000);
+});</script>
+<header class="">
+
+	<!--div id="right">
+		<div id="slog" >
+          
+      	</div>
+    </div>
+    <div id="left">
+		<ul >
+			<li> <a href="">+00 (123) 456 7890</a></li>
+			<li><a href="">INFO@DOMAIN.COM</a></li>
+		</ul>
+    </div-->
+</header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">FITNESSUP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">ACCEUIL <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#services" class="scrollTo">SERVICES</a>
+      <a class="nav-item nav-link" href="#activites" class="scrollTo">ACTIVté</a>
+      <a class="nav-item nav-link" href="#contact" class="scrollTo">CONTACT</a>
+    </div>
+  </div>
+</nav>
+
+<div class="ecran">
+				<DIV class="hero">
+				<div class="choix"> 
+				<img src="img/slogan.png"/> 
+				 <h1 class="word"></h1>
+				</div>
+				<!--div class="button">
+				<a href="#presente"><button class="but1"> Découvrir </button></a>
+				<a href="#contact"><button class="but2"> contact </button></a>
+			    </div-->
+			    </DIV>
+			</div>
+    <section class="type" id="services">
+		<div class="flex-container">
+		  <div class="flex-item">
+		  	<center ><i class="fas fa-cloud fa-2x"></i>
+            <p>
+		        <a href="#">SERVICES</a>
+		        <h5>Lorem ipsum dolor sit amet</h5></p> 
+		     </center>
+        </div>
+		  <div class="flex-item">
+		  	<center ><i class="fas fa-calendar fa-2x"></i>
+				<p>
+		       	<a href="#">HORAIRES</a>
+		         <h5>Lorem ipsum dolor sit amet</h5> </p>
+	         </center>
+		  </div>
+		  <div class="flex-item">
+		  	<center >
+		       <i class="fas fa-dumbbell fa-2x"></i>
+		       <p>
+		       <a href="#">STYLES </a>  
+		        <h5>Lorem ipsum dolor sit amet </h5></p>
+         </center>
+		  </div>
+		  <div class="flex-item">
+		  	<center ><i class="fas fa-mobile fa-2x"></i>
+
+		        <p>
+		        <a href="#">CONTACT</a>
+		        <h5>Lorem ipsum dolor sit amet</h5>  
+		        </p> 
+		    </center>
+		  </div>
+		</div>
+	</section>
+	<section class="activite" id="activites">
+		<div class="container">
+		<div class="row h-100 justify-content-center">
+			<div class="col-12 text-center">
+				<h1><span>N</span>os Activités</h1>
+			</div>
+			<div class="col-md-4 p-2">
+				<div class="card text-card">
+				  <img class="card-img" src="img/2.jpg" alt="Card image">
+				  <div class="card-img-overlay">
+				    <h2 class="card-title">CARDIO</h2>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 p-2">
+				  <div class="card text-card">
+				  <img class="card-img" src="img/1.jpg" alt="Card image">
+				  <div class="card-img-overlay">
+				    <h2 class="card-title">AQUAGYM</h2>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 p-2">
+				  <div class="card text-card">
+				  <img class="card-img" src="img/3.jpg" alt="Card image">
+				  <div class="card-img-overlay">
+				    <h2 class="card-title">MUSCU</h2>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+				  </div>
+				</div>
+			</div>
+      <div class="col-md-4 p-2">
+        <div class="card text-card">
+          <img class="card-img" src="img/2.jpg" alt="Card image">
+          <div class="card-img-overlay">
+            <h2 class="card-title">AEROBIC</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 p-2">
+        <div class="card text-card">
+          <img class="card-img" src="img/2.jpg" alt="Card image">
+          <div class="card-img-overlay">
+            <h2 class="card-title">ZUMBA</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 p-2">
+        <div class="card text-card">
+          <img class="card-img" src="img/2.jpg" alt="Card image">
+          <div class="card-img-overlay">
+            <h2 class="card-title">CARDIO</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          </div>
+        </div>
+      </div>
+		</div>
+	 </div>
+	</section>
+    <div class="icons">
+    	<ul><li>
+    	<a><img src="img/icons/fb.png"/></a></li><li>
+    	<a href=""><img src="img/icons/twit.png"/></a></li><li>
+    	<a><img src="img/icons/insta.png"/></a></li>
+        </ul>
+    </div>
+    <div class="wts">
+    	<a href="https://api.whatsapp.com/send?phone=39345678789715">
+    		<img src="img/icons/whatsapp.png"/ >
+    	</a>
+    </div>
+<section class="cont" id="contact">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1><span>C</span>ONTACT</h1>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col-6 col-bg">
+    </div>
+    <div class="col-6 bg-scndry">
+    <div class="row">
+    <div class="col-md-12 col-md-offset-3" id="form_container">
+        <form role="form" method="post">
+            <div class="row">
+                <div class="col-sm-12 form-group">
+                    <label for="name">
+                        Nom:</label>
+                    <input type="text" class="form-control" id="name" name="nom" required>
+                </div>
+                <div class="col-sm-12 form-group">
+                    <label for="email">
+                        Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 form-group">
+                    <label for="message">
+                        Message:</label>
+                    <textarea class="form-control" type="textarea" name="message" id="message" maxlength="6000" rows="7"></textarea>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12 form-group">
+                    <button type="submit" class="custom-btn pull-right" >
+                    Envoyer →</button>
+                </div>
+            </div>
+        </form>
+      </div>
+    </div>
+</div>
+</div>
+<?php
+    if(isset($_POST['message'])){
+        $entete  = 'MIME-Version: 1.0' . "\r\n";
+        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $entete .= 'From: ' . $_POST['email'] . "\r\n";
+
+        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        <p><b>Nom : </b>' . $_POST['nom'] . '<br>
+        <b>Email : </b>' . $_POST['email'] . '<br>
+        <b>Message : </b>' . $_POST['message'] . '</p>';
+
+        $retour = mail('fatima.aitmouss5@gmail.com', 'Envoi depuis page Contact', $message, $entete);
+        if($retour) {
+            echo
+            '<script>alert("Votre message a bien été envoyé");</script>';
+        }
+    }
+  ?>
+</section>
+	<footer class="bg-warning text-white text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    ©Design & dev : fatima-aitmouss
+  </div>
+  <!-- Copyright -->
+   
+	<script type="text/javascript" src="js/nav.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script type="text/javascript" src="js/index.js"></script>
+
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+</footer>
+</body>
+</html>
